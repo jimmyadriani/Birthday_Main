fa6d53ae4e57d4bc4baeb5cc0eb7a64b3e737b86
jimmyadriani <68756005+jimmyadriani@users.noreply.github.com>
2026-03-26 15:02:35 +0800

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { useState } from 'react';
+import DomeGallery from '@/components/DomeGallery';
+import InteractionFlow from '@/components/InteractionFlow';
+
+export default function Home() {
+  const [showGallery, setShowGallery] = useState(false);
+
+  const userImages = [
+    '/1.jpeg',
+    '/2.jpeg',
+    '/3.jpeg',
+    '/4.jpeg',
+    '/5.jpeg',
+    '/6.jpeg',
+    '/7.jpg',
+    '/8.jpg',
+    '/9.jpeg',
+    '/10.jpeg',
+    '/11.jpg',
+    '/12.jpeg',
+    '/13.jpg',
+    '/14.jpeg',
+    '/15.jpeg',
+	'/16.jpeg',
+	'/17.jpeg',
+	'/18.jpeg',
+	'/19.jpeg',
+	'/20.jpeg',
+  ];
+
+  return (
+    <main className="w-screen h-screen bg-[#060010]">
+      {!showGallery ? (
+        <InteractionFlow onFlowComplete={() => setShowGallery(true)} />
+      ) : (
+        <>
+          <audio src="/pretty.mp3" autoPlay loop className="hidden" />
+          <DomeGallery
+            images={userImages}
+            fit={0.8}
+            minRadius={600}
+            maxVerticalRotationDeg={0}
+            segments={34}
+            dragDampening={2}
+            grayscale={false}
+            autoRotationSpeed={0.1}
+          />
+        </>
+      )}
+    </main>
+  );
+}
